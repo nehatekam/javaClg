a4c792f2b3634bf96c9c85b08bbd885056c3519c
nehatekam <nehatekam237@gmail.com>
2024-08-08 03:01:32 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,13 @@
+public class even {
+    public static void main(String[] args) {
+        int n = 30;
+        for(int i=0;i<=n;i++){
+            if(i%2 == 0){
+                System.out.print(i+" ");
+                continue;
+               
+               }
+               
+            }
+        }
+    }
